119e0a8ac609d828f997c19e50a23a4ce0aacd02
tm1287 <tejas.maraliga@gmail.com>
2020-11-16 15:17:18 -0500

    Rename playerclient file

---
+++ playerclient/playerclient.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/select.h>
+#include <assert.h>
