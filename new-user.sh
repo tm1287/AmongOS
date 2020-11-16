0d1be96733b4d5d8b139cf171cfe41347929a4f5
tm1287 <tejas.maraliga@gmail.com>
2020-11-16 02:25:06 -0500

    Add already registered user logic

--- new-user.sh
+++ new-user.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-echo "Enter a username..."
+players="$(members players)"
 
-read username
+usernamegood=false
+
+while [ "$usernamegood" = false ]
+do
+    echo "Enter a username..."
+
+    read username
+
+    if [[ $players =~ $username ]]; then echo "Error: User already exists"; else usernamegood=true; fi
+done
 
 sudo useradd $username -g players
 
 sudo passwd $username
+
+if [[ $players == "" ]]; then
+    printf "\nYou are the host...\n"
+    sudo usermod -a -G players-host $username
+fi
 
 sudo -u $username /bin/bash
