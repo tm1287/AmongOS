47056c1fd95c9ff1ef1114d00d7c110812bc02ca
tm1287 <tejas.maraliga@gmail.com>
2020-11-16 21:55:27 -0500

    Add basic ncurses tui

--- playerclient/playerclient.c
+++ playerclient/playerclient.c
@@ -11,21 +11,32 @@
 #include <sys/select.h>
 #include <assert.h>
 
+#define C_START 0;
+#define C_MEETING 1;
+#define C_FINISHTASK 2;
+
+struct player_command {
+  int type;
+  char user[50];
+  char data[100];
+};
+
 int main(int argc, char* argv []) {
   int sid = socket(PF_INET, SOCK_STREAM, 0);
   struct sockaddr_in srv;
   struct hostent *server = gethostbyname("localhost");
+  char serverdata[1000];
+
 
   srv.sin_family = AF_INET;
   srv.sin_port = htons(27182);
   memcpy(&srv.sin_addr.s_addr,server->h_addr,server->h_length);
 
   int status = connect(sid, (struct sockaddr*)&srv, sizeof(srv));
-  printf("%d\n", status);
 
   while(1) {
-    sleep(2);
+
   }
-  
+
   return 0;
 }
